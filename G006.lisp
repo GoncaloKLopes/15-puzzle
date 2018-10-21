5ca72d364d4a5515278899b519bf249e0fabc22c
goncalochincho <goncalochincho@gmail.com>
2018-10-21 19:31:37 +0100

    Created operators (closes #9, closes #8, closes #7, closes #6, closes #5), corresponding tests and some aux functions

--- G006.lisp
+++ G006.lisp
@@ -5,14 +5,101 @@
 							(9 10 11 12)
 							(13 14 15 nil))))
 
-;;Check if state is the goal state.
-;;
-;;Arguments:
-;;
-;;state -- The state being tested.
-;;
-;;Return:
-;;T if it is, NIL otherwise.
+
+(defun get-position (state)
+	"Get the empty position.
+	 Arguments:
+	 * state -- A state.
+	 Return:
+	 * A pair corresponding to the position of the player."
+	(let ((n-rows (car (array-dimensions state)))
+		  (n-columns (car (cdr (array-dimensions state)))))
+		(dotimes (i n-rows) ;go through each position (i, j) to check if it's nil
+			(dotimes (j n-columns)
+				(if (null (aref state i j))
+					(return-from get-position (cons i j)))))))
+
+(defun copy-array (array)
+	"Copy an array.
+	 Arguments:
+	 * array -- The array to be copied.
+	 Return:
+	 * A copy of array."
+	 (let ((new-array (make-array (array-dimensions array))))
+	 	(dotimes (i (car (array-dimensions array)))
+	 			(dotimes (j (car (cdr (array-dimensions array))))
+	 				(setf (aref new-array i j) (aref array i j))))
+	 	new-array))
+
 (defun goalp (state)
+	"Check if state is the goal state.
+	 Arguments:
+	 * state -- The state being tested.
+	 Return:
+	 * T if it is, NIL otherwise."
 	(equalp state +goal-state+))
 
+
+(defun operator-move-up (state)
+	"Apply the move-up operator.
+	 Arguments:
+	 * state -- The state being changed.
+	 Return:
+	 * The new resulting state or state if already at the top row."
+	 (let ((row (car (get-position state)))
+	 	   (col (cdr (get-position state))))
+	 	(if (equalp row 0) ;if top row
+			state 
+			(let ((result (copy-array state))) ;new var, don't change original state
+			(progn
+				(setf (aref result row col) (aref state (- row 1) col))
+				(setf (aref result (- row 1) col) nil)
+				result)))))
+
+(defun operator-move-down (state)
+	"Apply the move-down operator.
+	 Arguments:
+	 * state -- The state being changed.
+	 Return:
+	 * The new resulting state or state if already at the bottom row."
+	 (let ((row (car (get-position state)))
+	 	   (col (cdr (get-position state))))
+	 	(if (equalp row (- (car (cdr (array-dimensions state))) 1)) ;if bottom row
+			state 
+			(let ((result (copy-array state))) ;new var, don't change original state
+			(progn
+				(setf (aref result row col) (aref state (+ row 1) col))
+				(setf (aref result (+ row 1) col) nil)
+				result)))))
+
+(defun operator-move-left (state)
+	"Apply the move-left operator.
+	 Arguments:
+	 * state -- The state being changed.
+	 Return:
+	 * The new resulting state or state if already at left-est column."
+	 (let ((row (car (get-position state)))
+	 	   (col (cdr (get-position state))))
+	 	(if (equalp col 0) ;if left-est column
+			state 
+			(let ((result (copy-array state))) ;new var, don't change original state
+			(progn
+				(setf (aref result row col) (aref state row (- col 1)))
+				(setf (aref result row (- col 1)) nil)
+				result)))))
+
+(defun operator-move-right (state)
+	"Apply the move-right operator.
+	 Arguments:
+	 * state -- The state being changed.
+	 Return:
+	 * The new resulting state or state if already at right-est column."
+	 (let ((row (car (get-position state)))
+	 	   (col (cdr (get-position state))))
+	 	(if (equalp col (- (car (cdr (array-dimensions state))) 1)) ;if right-est column
+			state 
+			(let ((result (copy-array state))) ;new var, don't change original state
+			(progn
+				(setf (aref result row col) (aref state row (+ col 1)))
+				(setf (aref result row (+ col 1)) nil)
+				result)))))
--- tests.lisp
+++ tests.lisp
@@ -13,16 +13,112 @@
 			(9 10 11 12)
 			(13 14 15 nil))))
 
+(setf a3 (make-array '(4 4)
+			:initial-contents '((nil 2 3 4)
+			(5 6 7 8)
+			(9 10 11 12)
+			(13 14 15 1))))
+
+(setf a4 (make-array '(4 4)
+			:initial-contents '((1 2 3 4)
+			(5 6 7 8)
+			(13 nil 10 11)
+			(14 9 15 12))))
+
+(setf a5 (make-array '(4 4)
+			:initial-contents '((1 2 3 4)
+			(5 6 7 8)
+			(nil 13 10 11)
+			(14 9 15 12))))
+
+;;;get-position
+(let ((res "")
+	 (test (get-position a1)))
+	(if (equalp test '(3 . 1))
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "get-position test ~A!~%" res))
+
+;;;copy-array
+(let ((res "")
+	 (test (copy-array a1)))
+	(if (and (equalp test a1) (not (eq test a1)))
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "copy-array45 test ~A!~%" res))
 ;;;goalp
-(let((res ""))
+(let ((res ""))
 	(if (goalp a2)
 		(setf res "passed")
 		(setf res "failed"))
 	(format t "goalp same test ~A!~%" res))
 
-(let((res ""))
+(let ((res ""))
 	(if (goalp a1)
 		(setf res "failed")
 		(setf res "passed"))
 	(format t "goalp different test ~A!~%" res))
+
+;;operator-move-up
+(let ((res "")
+	  (test (operator-move-up a3)))
+	(if (equalp test a3)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-up no move test ~A!~%" res))
+
+(let ((res "")
+	  (test (operator-move-up a1)))
+	(if (equalp test a4)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-up yes move test ~A!~%" res))
+
+
+;;operator-move-down
+(let ((res "")
+	  (test (operator-move-down a1)))
+	(if (equalp test a1)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-down no move test ~A!~%" res))
+
+(let ((res "")
+	  (test (operator-move-down a4)))
+	(if (equalp test a1)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-down yes move test ~A!~%" res))
+
+
+;;operator-move-left
+(let ((res "")
+	  (test (operator-move-left a5)))
+	(if (equalp test a5)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-left no move test ~A!~%" res))
+
+(let ((res "")
+	  (test (operator-move-left a4)))
+	(if (equalp test a5)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-left yes move test ~A!~%" res))
+
+
+;;operator-move-right
+(let ((res "")
+	  (test (operator-move-right a2)))
+	(if (equalp test a2)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-right no move test ~A!~%" res))
+
+(let ((res "")
+	  (test (operator-move-right a5)))
+	(if (equalp test a4)
+		(setf res "passed")
+		(setf res "failed"))
+	(format t "operator-move-right yes move test ~A!~%" res))
 
